08e6d6a33477b4e1cf133c7e75700f01a85370b7
Kent Saitoh <31811230+Kentoooooooo@users.noreply.github.com>
2019-08-19 21:27:54 +0900

    d

---
+++ mysite/myapps/calender/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import generic
+from . import mixins
+
+# Create your views here.
+
+
+class MonthCalendar(mixins.MonthCalendarMixin, generic.TemplateView):
+    """月間カレンダーを表示するビュー"""
+    template_name = 'calender/month.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_month_calendar()
+        context.update(calendar_context)
+        print('context is')
+        print(context)
+        return context
